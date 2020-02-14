e5b402bde1eccefab539527d7ee7edd2206f2f88
LiBigTree <13289771760@163.com>
2020-02-14 12:19:42 +0800

    均值滤波

---
+++ 2.4 均值滤波/code.m
@@ -0,0 +1,25 @@
+clc;
+clear;
+close all;
+
+img = imread('1.jpg');
+
+gray = rgb2gray(img);
+figure('name', 'gray');
+imshow(gray);
+
+[m, n] = size(gray);
+
+avg_filter = gray;
+for i = 2:m-1
+    for j = 2:n-1
+        
+        filter_con = gray(i-1:i+1, j-1:j+1);
+        k = sum(sum(filter_con))/9;
+        avg_filter(i, j) = k;
+    
+    end
+end
+
+figure('name', 'avg_filter');
+imshow(avg_filter);
